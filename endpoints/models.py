58832f2dbba3f50427230137673bd55b7bcf08d0
Leonard.Cristescu <60926@gymnasium.ax>
2024-10-31 15:51:59 +0200

    Models.py begins

--- endpoints/models.py
+++ endpoints/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Activity(models.model):
+    activity_name=models.CharField(max_length=100)
+    activity_hasTime=models.BooleanField()
+    activity_hasDistance=models.BooleanField()
+    activity_hasDistanceUnit=models.Choices()
+    activity_UserId=models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.activity_name
+
+
+class Workout(models.model):
+    workout_date=models.DateField()
+    user_id=models.ForeignKey()
+    activity_id=models.ForeignKey()
+    workout_description=models.TextField()
+    workout_time=models.DurationField()
+    workout_distance=models.DecimalField()
+    workout_rpe=models.IntegerField()
+    
+    def __str__(self):
+        return self
+
